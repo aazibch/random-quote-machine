8de3f53d669a57049521756609880ff0e8ad7582
Aazib Chaudhry <aazibch47@gmail.com>
2021-09-23 19:38:07 +0500

    Modified project to conform to new FCC requirements

--- README.md
+++ README.md
@@ -1,4 +1,29 @@
 # Random Quote Machine
-View random quotations.
 
-The application incorporates data from [forismatic.com's API](https://forismatic.com/en/api/).
+View random quotations from [forismatic.com's API](https://forismatic.com/en/api/).
+
+This project is part of [freeCodeCamp](https://www.freecodecamp.org/)'s curriculum.
+
+## User Stories
+
+1. I can see a wrapper element with a corresponding id="quote-box".
+
+2. Within #quote-box, I can see an element with a corresponding id="text".
+
+3. Within #quote-box, I can see an element with a corresponding id="author".
+
+4. Within #quote-box, I can see a clickable element with a corresponding id="new-quote".
+
+5. Within #quote-box, I can see a clickable a element with a corresponding id="tweet-quote".
+
+6. On first load, my quote machine displays a random quote in the element with id="text".
+
+7. On first load, my quote machine displays the random quote's author in the element with id="author".
+
+8. When the #new-quote button is clicked, my quote machine should fetch a new quote and display it in the #text element.
+
+9. My quote machine should fetch the new quote's author when the #new-quote button is clicked and display it in the #author element.
+
+10. I can tweet the current quote by clicking on the #tweet-quote a element. This a element should include the "twitter.com/intent/tweet" path in its href attribute to tweet the current quote.
+
+11. The #quote-box wrapper element should be horizontally centered. Please run tests with browser's zoom level at 100% and page maximized.
--- script.js
+++ script.js
@@ -1,28 +1,35 @@
-/*jshint browser: true, devel: true, jquery: true*/
 $(document).ready(function () {
-    getData();
-    $("#new-quote-button").on("click", getData);
+  getData();
+  $('#new-quote').on('click', getData);
 
-    $("#new-quote-button, #tweet-button").on("click", function () {
-        this.blur();
-    });
+  $('#new-quote, #tweet-quote').on('click', function () {
+    this.blur();
+  });
 
-    function getData () {
-        $.getJSON("https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?", function (data) {
-            var encodedURL;
+  function getData() {
+    $.getJSON(
+      'https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?',
+      function (data) {
+        var encodedURL;
 
-            if (data.quoteText.length + data.quoteAuthor.length + 5 >= 140) {
-                getData();
-            } else {
-                $("#quote").text(data.quoteText);
-                if (data.quoteAuthor.length === 0) {
-                    $("#quote-author").html('<span id="text-anonymous">Anonymous</span>');
-                } else {
-                    $("#quote-author").text(data.quoteAuthor);
-                }
-                encodedURL = encodeURIComponent('"' + data.quoteText + '"' + ' - ' + data.quoteAuthor);
-                $("#tweet-button").attr("href", "https://twitter.com/intent/tweet?text=" + encodedURL);
-            }
-        });
-    }
+        if (data.quoteText.length + data.quoteAuthor.length + 5 >= 140) {
+          getData();
+        } else {
+          $('#text').text(data.quoteText);
+          if (data.quoteAuthor.length === 0) {
+            $('#author').html('<span id="text-anonymous">Anonymous</span>');
+          } else {
+            $('#author').text(data.quoteAuthor);
+          }
+          encodedURL = encodeURIComponent(
+            '"' + data.quoteText + '"' + ' - ' + data.quoteAuthor
+          );
+          $('#tweet-quote').attr(
+            'href',
+            'https://twitter.com/intent/tweet?text=' + encodedURL
+          );
+        }
+      }
+    );
+  }
 });
